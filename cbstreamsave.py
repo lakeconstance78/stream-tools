f940196db72a39708aac8892023f814acd81da15
Ben <lakeconstance78@wolke7.net>
2023-03-07 09:05:50 +0100

    Update cbstreamsave.py
    - cleanup

--- cbstreamsave.py
+++ cbstreamsave.py
@@ -1,16 +1,11 @@
 import os
 import sys
 import time
-import random
-import datetime
-from time import sleep
-from datetime import date
 from datetime import datetime
 from bs4 import BeautifulSoup
 from termcolor import colored, cprint
 
 import json
-import schedule
 
 from multiprocessing import Queue, Process
 
@@ -25,9 +20,6 @@ driver = Driver(uc=True)
 def cls():
     os.system('cls' if os.name == 'nt' else 'clear')
 
-def randomwaiting():
-    time.sleep(random.randint(2,4))
-
 def writestreamtofile(queue):
     item = queue.get()
     # getting variables from queue witout brackets
@@ -37,7 +29,8 @@ def writestreamtofile(queue):
     directory = './streamcapture/' + broadcaster_stream + '/' + str(datetime.now().strftime("%d-%m-%Y")) + '/'
     os.makedirs(directory, mode=0o777, exist_ok=True)
     filename = directory + str(datetime.now().strftime("%d-%m-%Y_%H-%M-%S")) + "_" + broadcaster_stream + '.mp4'
-    
+    filename_log = directory + str(datetime.now().strftime("%d-%m-%Y_%H-%M-%S")) + "_" + broadcaster_stream + '.log'
+
     if str(broadcaster_stream) or str(broadcaster_hls) not in broadcasterm3ufile:
         with open('broadcasters.m3u','a', encoding='utf-8') as outputfile:
             os.chmod('broadcasters.m3u', 0o666)
@@ -46,50 +39,36 @@ def writestreamtofile(queue):
             outputfile.write('\n')
     try:
         session = streamlink.Streamlink()
-        #https://streamlink.github.io/cli.html#cmdoption-http-no-ssl-verify
-        session.set_option('http-header', {'User-Agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36'})
-        session.set_option('http-header', {'referer=https://chaturbate.eu/'})
-        session.set_option('http-no-ssl-verify', 'true')
-        session.set_option('default-stream', {'480p,360p'})
-        # TEST OPTIONS
-        #--stream-timeout TIMEOUT
-        #Timeout for reading data from streams
-        session.set_option('stream-timeout', 10)
-        #--stream-segment-threads THREADS
-        #The size of the thread pool used to download segments. Minimum value is 1 and maximum is 10
-        session.set_option('stream-segment-thread', 3)
-        #--stream-segment-timeout TIMEOUT
-        #Segment connect and read timeout.
-        session.set_option('stream-segment-timeout', 10)
-        #--stream-segment-attempts ATTEMPTS
-        #How many attempts should be done to download each segment before giving up.
-        session.set_option('stream-segment-attempts', 2)
-        #--ringbuffer-size SIZE
-        #The maximum size of the ringbuffer. Mega- or kilobytes can be specified via the M or K suffix respectively.
-        #The ringbuffer is used as a temporary storage between the stream and the player. 
-        #This allows Streamlink to download the stream faster than the player which reads the data from the ringbuffer.
-        #session.set_option('ringbuffer-size', '8192k')
-        #--retry-streams DELAY
-        #Retry fetching the list of available streams until streams are found while waiting DELAY second(s) between each attempt. 
-        #If unset, only one attempt will be made to fetch the list of streams available.
-        session.set_option('retry-streams', 10)
-        #--retry-max COUNT
-        #When using --retry-streams, stop retrying the fetch after COUNT retry attempt(s). 
-        #Fetch will retry infinitely if COUNT is zero or unset.
-        session.set_option('retry-max', 2)
-        #--retry-open ATTEMPTS
-        #After a successful fetch, try ATTEMPTS time(s) to open the stream until giving up.
-        session.set_option('retry-open', 2)
-        #--hls-playlist-reload-attempts ATTEMPTS
-        #How many attempts should be done to reload the HLS playlist before giving up.
-        session.set_option('hls-playlist-reload-attempts', 2)
-        # TEST OPTIONS
-        #session.set_option('hls-playlist-stream-data', 'true')        
-        #session.set_option('http-timeout', 30)
-
+        session.set_option('subprocess-errorlog', 'true')
+        session.set_option('subprocess-errorlog-path', 'filename_log')
+        session.set_option('ffmpeg-verbose', 'true')
+        session.set_option('ffmpeg-verbose-path', 'filename_log')
+        
+        # General timeout used by all HTTP/HTTPS requests, except the ones covered by other options
+        session.set_option('http-timeout', 60)
+                
+        # Max number of HLS playlist reload attempts before giving up
+        session.set_option('hls-playlist-reload-attempts', 10)
+        # Override the HLS playlist reload time, either in seconds (float) or as a str keyword
+        #   segment:      duration of the last segment
+        #   live-edge:    sum of segment durations of the hls-live-edge value minus one
+        #   default:      the playlist's target duration):
+        session.set_option('hls-playlist-reload-time', 'segment')
+ 
         streams = session.streams(broadcaster_hls)
-        stream = streams['480p']
+
+        try: 
+            stream = streams['480p']
+        except:
+            try:
+                print(colored(broadcaster_stream + ' 480p not available, switching to 360p', 'yellow'))
+                stream = streams['360p']
+            except:
+                print(colored(broadcaster_stream + ' 360p not available, switching to worst', 'yellow'))
+                stream = streams['worst']
+
         fd = stream.open()
+
         if fd != 0:
             with open(filename, 'wb') as f:
                 os.chmod(filename, 0o666)
@@ -100,26 +79,29 @@ def writestreamtofile(queue):
                 except:
                     f.close()
         else: already_in_queue.remove(broadcaster_stream)
-    #except streamlink.exceptions.StreamlinkError as e:
-    except BaseException as e:
+
+    except streamlink.exceptions.StreamlinkError as streamlink_error:
+        if '403 Client Error' in str(streamlink_error):
+            print(colored(broadcaster_stream + ' Streamlink error: 403 Client Error. Broadcaster is probably offline, in a ticket show or private', 'red'))
+        elif 'Unable to open URL' in str(streamlink_error):
+            print(colored(broadcaster_stream + ' Streamlink error: Unable to open URL (Read-Timeout). Broadcaster is probably offline', 'red'))
+        else:
+            print(colored(broadcaster_stream + ' Streamlink error: ' + str(streamlink_error), 'red'))
+
+    except Exception as e:
         already_in_queue.remove(broadcaster_stream)
         if 'ssl' in str(e):
             #print(e)
-            print(colored(broadcaster_stream + ': some ssl error opening the stream', 'red'))
-        elif 'Unable to open URL' in str(e):
-            print(colored(broadcaster_stream + ': unable to open URL. Broadcaster is probably offline', 'red'))
-        #elif '404' in str(e):
-            #print(colored(broadcaster_stream + ': failed to reload playlist. Broadcaster is probably offline', 'red'))
+            print(colored(broadcaster_stream + ' some ssl error opening the stream', 'red'))
         else: 
-            print(colored(broadcaster_stream + ': some network error occured', 'red'))
+            print(colored(broadcaster_stream + ' some network error occured', 'red'))
             print(e)
-            
+
 def check_if_online(broadcaster):
     apiurl = ('https://chaturbate.com/api/chatvideocontext/' + broadcaster)
     try:
         driver.get(apiurl)
         r = driver.page_source
-        # parsing json with soup
         soup = BeautifulSoup(r, 'html.parser')
         souptext = soup.text
         j = json.loads(souptext)
@@ -144,27 +126,27 @@ def check_if_online(broadcaster):
         else:
             if broadcaster in already_in_queue:
                 already_in_queue.remove(broadcaster)
-                print(colored(broadcaster, 'red') + colored(' is offilne, removing from queue (ELSE)', 'cyan'))
-    except: #Exception as e:
-        #if 'hls_source' not in e: 
-        #print(e)
+                print(colored(broadcaster, 'red') + colored(' is offline', 'red'))
+    except Exception as e:
         if broadcaster in already_in_queue:
             already_in_queue.remove(broadcaster)
-            print(colored(broadcaster, 'red') + colored(' is offilne, removing from queue (EXCEPT)', 'cyan'))
+            print(colored(broadcaster + ' is offline, removing from queue (EXCEPT)', 'red'))
+    except KeyboardInterrupt:
+        print('KeyboardInterrupt in check_if_online')
+        if stream:
+            stream.close()
+        if driver:
+            driver.close()
+        sys.exit(1)
 
 def checkwantlist():
     try:
-        start_time = datetime.now()
         while True:
-            #start_time = datetime.now()
             print('Checking online broadcasters from wantlist.txt')
             for broadcaster in wantlist:
                 check_if_online(broadcaster)
-            end_time = datetime.now()
-            duration = int((end_time - start_time).total_seconds())
-            time.sleep(duration/100)
     except KeyboardInterrupt:
-        print('keyboard interrupt')
+        print('KeyboardInterrupt in checkwantlist')
         if stream:
             stream.close()
         if driver:
@@ -173,30 +155,29 @@ def checkwantlist():
         sys.exit(1)
 
 def showinfos():
-    cls()
-    print(str(str(datetime.now().strftime("%d-%m-%Y %H:%M:%S"))) + ' Queue [actual: '+str(len(already_in_queue)) +'/maximum: ' + str(maxqueue)+']: ' + str(already_in_queue) + ' Duration: ' + str(duration) + 's')
+    #cls()
+    print(str(str(datetime.now().strftime("%d-%m-%Y %H:%M:%S"))) + ' Queue [actual: '+str(len(already_in_queue)) +'/maximum: ' + str(maxqueue)+']: ' + str(already_in_queue))
     if len(already_in_queue) >= maxqueue:
         print(colored('queue is full', 'red'))
 
 if __name__=='__main__':
     try:
         cls()
-        maxqueue = 5
+        maxqueue = 200
         stream = 0
         fd = 0
         already_in_queue = []
         broadcasterm3ufile = []
         broadcasterm3u = 0
-        duration = 600
         queue = Queue(maxsize=maxqueue)
-        queue2 = Queue(maxsize=maxqueue)
-        with open('wanted.txt', 'r') as wantlistfile:
+        with open('cbfollowed.txt', 'r') as wantlistfile:
             wantlist = wantlistfile.read().splitlines()
         if os.path.exists('broadcasters.m3u'):
             with open('broadcasters.m3u', 'r') as broadcasterm3ufile:
                 broadcasterm3u = broadcasterm3ufile.read().splitlines()
         checkwantlist()
     except KeyboardInterrupt:
+        print('KeyboardInterrupt in main')
         if stream:
             stream.close()
         if driver:
